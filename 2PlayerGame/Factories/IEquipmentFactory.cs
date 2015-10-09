4827dd8c26e2395e77827983a1fe4d4e58a4594b
mnovera <netzon.miguel@gmail.com>
2015-10-09 18:09:20 +0800

    New classes

---
+++ 2PlayerGame/Factories/IEquipmentFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace _2PlayerGame
+{
+  public interface IEquipmentFactory
+  {
+    IEquipment CreateEquipment();
+  }
+}
---
+++ 2PlayerGame/Factories/WeaponFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace _2PlayerGame
+{
+  public class WeaponFactory : IEquipmentFactory
+  {
+
+    public IEquipment CreateEquipment()
+    {
+      IOutputService message = new ConsoleOutput();
+      IEquipment weapon = null;
+
+      string optionWeap = message.WeaponPrompt();
+      Selection selectWeap = (Selection)Enum.Parse(typeof(Selection),
+                               optionWeap, true);
+
+      switch (selectWeap)
+      {
+        case Selection.A:
+          {
+            weapon = new Knife();
+            break;
+          }
+
+        case Selection.B:
+          {
+            weapon = new Sword();
+            break;
+          }
+
+        case Selection.C:
+          {
+            weapon = new Blade();
+            break;
+          }
+
+        case Selection.D:
+          {
+            weapon = new Katana();
+            break;
+          }
+      }
+      return weapon;
+    }
+
+  }
+}
